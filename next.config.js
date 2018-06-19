bc95b15528c7d346f04c4a4b7a87aa5bc1213e0a
Ahmed Alahmady <aalahmady@gmail.com>
2018-06-19 11:27:54 -0500

    initial commit -- config files

---
+++ next.config.js
@@ -0,0 +1,69 @@
+const { IgnorePlugin } = require('webpack')
+const Dotenv = require('dotenv-webpack')
+const optimizedImages = require('next-optimized-images')
+const webpack = require('webpack')
+const withPlugins = require('next-compose-plugins')
+
+const nextConfig = {
+  webpack: (config, { dev }) => {
+    const webpackPlugins = config.plugins
+    const webpackRules = config.module.rules
+    const originalEntry = config.entry
+
+    config.entry = async () => {
+      const entries = await originalEntry()
+
+      // loading polyfills before bundle
+      if (entries['main.js']) {
+        entries['main.js'].unshift('./static/scripts/polyfill.js')
+      }
+
+      return entries
+    }
+
+    const customWebpackConfig = {
+      plugins: {
+        base: [
+          new IgnorePlugin(
+            /^\.\/locale$/,
+            /apollo-server-express$/,
+            /body-parser$/,
+            /compression$/,
+            /cors$/,
+            /emotion-server$/,
+            /graphql$/,
+            /graphql-tools$/,
+            /express$/,
+            /moment$/,
+            /signale$/,
+            /uuid$/
+          ),
+          new Dotenv({ path: './.env', systemvars: true })
+        ],
+        prod: [
+          new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"'
+          })
+        ]
+      },
+      rules: {
+        base: [
+          // url loader no longer needed after using next-images
+        ]
+      }
+    }
+
+    if (!dev) {
+      webpackPlugins.push(...customWebpackConfig.plugins.prod)
+    }
+
+    webpackRules.push(...customWebpackConfig.rules.base)
+    webpackPlugins.push(...customWebpackConfig.plugins.base)
+
+    config.stats = { colors: true }
+
+    return config
+  }
+}
+
+module.exports = withPlugins([optimizedImages], nextConfig)
